26204011a058be9642e840902ce196658647b2e0
ritabc <ritabc@gmail.com>
2018-07-23 15:02:39 -0700

    Pass 1st test

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -3,7 +3,7 @@ require('spec_helper')
 describe('Space') do
   context('after running seeds file') do
     it('will find the space by coordinates and return the room type') do
-      expect(Space.find_by(coordinates: 'I10')).to(eq('Study'))
+      expect((Space.find_by(coordinates: 'I10')).space_type).to(eq('Study'))
     end
   end
 end
