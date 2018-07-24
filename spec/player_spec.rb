c7479a100403ffcf2c851e21d49ba70c0f75988f
ritabc <ritabc@gmail.com>
2018-07-24 10:20:36 -0700

    Pass and check Player#end_turn method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,13 +1,21 @@
 require('spec_helper')
 
 describe(Player) do
-  context('end of turn') do
+  context('#end_turn') do
     it('will switch turns from the current player to the next player') do
-      player1 = Player.create({:turn => false})
-      player2 = Player.create({:turn => true})
-      player3 = Player.create({:turn => false})
-      player2.end_turn
-      expect(Player.find(player3.id).turn).to(eq(true))
+      player1 = Player.find_by(name: 'Player1')
+      player2 = Player.find_by(name: 'Player2')
+      player3 = Player.find_by(name: 'Player3')
+      player4 = Player.find_by(name: 'Player4')
+      player1.update(turn: 'f')
+      player2.update(turn: 'f')
+      player3.update(turn: 'f')
+      player4.update(turn: 't')
+      player4.end_turn
+      player1 = Player.find_by(name: 'Player1')
+      player4 = Player.find_by(name: 'Player4')
+      expect(player1.turn).to(eq(true))
+      expect(player4.turn).to(eq(false))
     end
   end
   context '#roll_dice' do
