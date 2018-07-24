26caaafab4fcf3e30d80115206c218b8bff46626
Nate Cottle <cottle.nate@gmail.com>
2018-07-23 21:01:28 -0700

    Add game class to reset the entire game, add passing tests for game class.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
-# describe Game do
-#   context '.full_reset' do
-#     it 'will fully reset the game, reset cards, spaces, and players' do
-#       player1 = Player.create({:name => 'Player 1', :turn => 'f', :dice_roll => 4, :guess => nil})
-#       Card.murder
-#       Card.deal_cards
-#       Game.full_reset
-#       expect(Player.all).to(eq(nil))
-#     end
-#   end
-# end
+describe Game do
+  context '.full_reset' do
+    it 'will fully reset the game, reset cards, spaces, and players' do
+      Card.murder
+      Card.deal_cards
+      Game.full_reset
+      expect(Player.all).to(eq([]))
+    end
+  end
+  context '.parital_reset' do
+    it 'will delete everything but players' do
+      Card.murder
+      Card.deal_cards
+      Game.partial_reset
+      expect(Player.all).not_to(eq([]))
+    end
+  end
+end
