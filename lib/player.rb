5094d137de0095acd3510bb5ab7d3aca9773ce00
ritabc <ritabc@gmail.com>
2018-09-01 17:12:40 -0700

    Fix whitespace tabs

--- lib/player.rb
+++ lib/player.rb
@@ -41,15 +41,15 @@ class Player < ActiveRecord::Base
     available_spaces = self.available_spaces(original_coords)
     new_space = Space.find_by(coordinates: new_coords)
     if !roll.nil? && roll > 0
-        if available_spaces.include?(new_space.coordinates)
-          new_space.update(player_id: self.id)
-          original_space.update(player_id: nil)
-          roll -= 1
-          self.update(dice_roll: roll)
-        end
-        if all_room_coords.include?(new_coords)
-          guess_allowed = true
-        end
+      if available_spaces.include?(new_space.coordinates)
+        new_space.update(player_id: self.id)
+        original_space.update(player_id: nil)
+        roll -= 1
+        self.update(dice_roll: roll)
+      end
+      if all_room_coords.include?(new_coords)
+        guess_allowed = true
+      end
     end
   end
 
